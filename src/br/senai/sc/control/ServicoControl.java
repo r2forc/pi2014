ff20dad22e1d66f2e9c4282ed5e51bc15d2692fc
Felipe Bento <felipeaugustobento@hotmail.com>
2014-11-11 19:18:59 -0200

    Adicionado pesquisa com filtro no servicoDAO

--- src/br/senai/sc/control/ServicoControl.java
+++ src/br/senai/sc/control/ServicoControl.java
@@ -3,12 +3,18 @@ package br.senai.sc.control;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import br.senai.sc.dao.ClienteDAO;
 import br.senai.sc.dao.ServicoDAO;
+import br.senai.sc.model.Cliente;
 import br.senai.sc.model.Servico;
 
 public class ServicoControl {
 	public ArrayList<Servico> showAllServicos() throws ClassNotFoundException,
 			SQLException {
 		return ServicoDAO.getInstace().showAllServicos();
+	}
+	public ArrayList<Servico> showFilterServicos(String column, String value)
+			throws ClassNotFoundException, SQLException {
+		return ServicoDAO.getInstace().showFilterServicos(column, value);
 	}
 }
--- src/br/senai/sc/dao/ServicoDAO.java
+++ src/br/senai/sc/dao/ServicoDAO.java
@@ -8,11 +8,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import br.senai.sc.model.Cliente;
+import br.senai.sc.model.ConnectionUtil;
 import br.senai.sc.model.Servico;
 
 public class ServicoDAO {
 	private static ServicoDAO instance;
 	private ArrayList<Servico> listaServicos = new ArrayList<Servico>();
+	private Connection con = ConnectionUtil.getConnection();
 
 	public static ServicoDAO getInstace() {
 		if (instance == null) {
@@ -26,7 +28,7 @@ public class ServicoDAO {
 
 		String query = "SELECT * FROM Servico ORDER BY descricao ASC;";
 
-		PreparedStatement stmt = obterConexao().prepareStatement(query);
+		PreparedStatement stmt = con.prepareStatement(query);
 
 		ResultSet rs = stmt.executeQuery();
 
@@ -46,23 +48,29 @@ public class ServicoDAO {
 		return listaServicos;
 	}
 
-	public Integer verificaExistencia(Servico servico) {
-		for (int i = 0; i < this.listaServicos.size(); i++) {
-			if (servico.getId().equals(this.listaServicos.get(i).getId())) {
-				return i;
-			}
+	public ArrayList<Servico> showFilterServicos(String column, String value)
+			throws ClassNotFoundException, SQLException {
+
+		String query = "SELECT * FROM Servico WHERE " + column + " LIKE '%"
+				+ value + "%' ORDER BY descricao ASC;";
+
+		PreparedStatement stmt = con.prepareStatement(query);
+
+		ResultSet rs = stmt.executeQuery();
+
+		Servico sRetorno = null;
+		ArrayList<Servico> listaServicos = new ArrayList<>();
+
+		while (rs.next()) {
+
+			sRetorno = new Servico();
+
+			sRetorno.setId(rs.getInt("id"));
+			sRetorno.setDescricao(rs.getString("descricao"));
+			sRetorno.setValorUnt(rs.getDouble("valorUnt"));
+
+			listaServicos.add(sRetorno);
 		}
-		return null;
-	}
-
-	public static Connection obterConexao() throws ClassNotFoundException,
-			SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-
-		String url = "jdbc:mysql://localhost:3306/r2forc";
-
-		Connection con = DriverManager.getConnection(url, "root", "26051993");
-
-		return con;
+		return listaServicos;
 	}
 }
