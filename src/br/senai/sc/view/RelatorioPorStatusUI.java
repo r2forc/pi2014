76bf83c3c5f19e26b2084d258486206b804ae86f
Fabrício <Fabrcio@192.168.25.190>
2014-11-11 21:30:01 -0200

    Tela Relatorio por Status

---
+++ src/br/senai/sc/view/RelatorioPorStatusUI.java
@@ -0,0 +1,49 @@
+package br.senai.sc.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
+
+public class RelatorioPorStatusUI extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RelatorioPorStatusUI frame = new RelatorioPorStatusUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RelatorioPorStatusUI() {
+		getContentPane().setBackground(new Color(0, 255, 0));
+		setTitle("Relat\u00F3rio por Status");
+		setBorder(null);
+		setBounds(0, 0, 1000, 600);
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 1000, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 573, Short.MAX_VALUE)
+		);
+		getContentPane().setLayout(groupLayout);
+
+	}
+
+}
