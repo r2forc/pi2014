5952dd89c10b43a987129ec6180990d0e77876d9
Felipe Bento <felipeaugustobento@hotmail.com>
2014-11-12 19:05:56 -0200

    teste

---
+++ src/br/senai/sc/tests/TesteViews.java
@@ -0,0 +1,10 @@
+package br.senai.sc.tests;
+
+import br.senai.sc.view.OrcamentoUI;
+
+public class TesteViews {
+
+	public static void main(String[] args) {
+		OrcamentoUI orc = new OrcamentoUI();
+	}
+}
