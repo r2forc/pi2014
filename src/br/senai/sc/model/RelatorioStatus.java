7b87b952d93ff9bcc0db26146dd19a96b4e3429c
Fabrício <Fabrcio@192.168.25.190>
2014-11-13 09:48:22 -0200

    Model Relatorio por Status - Fabrício

---
+++ src/br/senai/sc/model/RelatorioStatus.java
@@ -0,0 +1,55 @@
+package br.senai.sc.model;
+
+import java.util.Date;
+
+public class RelatorioStatus {
+	private Cliente cliente;
+	private int status;
+	private Date data;
+	private Double valor;
+
+	public RelatorioStatus(Cliente cliente, int status, Date data, Double valor) {
+
+		this.cliente = cliente;
+		this.status = status;
+		this.data = data;
+		this.valor = valor;
+	}
+
+	public RelatorioStatus() {
+
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+}
