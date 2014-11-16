ef6523786319d38739071e7a6e6b4f50276e801b
Fabrício <Fabrcio@192.168.25.190>
2014-11-16 20:58:01 -0200

    RelatorioTableModel

---
+++ src/br/senai/sc/utils/RelatorioTableModel.java
@@ -0,0 +1,103 @@
+package br.senai.sc.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import br.senai.sc.model.RelatorioStatus;
+
+public class RelatorioTableModel extends AbstractTableModel {
+	private static final long serialVersionUID = 1L;
+	private static final int COL_CLIENTE = 0;
+	private static final int COL_STATUS = 1;
+	private static final int COL_DATA = 2;
+	private static final int COL_VALOR = 3;
+
+	private List<RelatorioStatus> valores;
+
+	// Esse é um construtor, que recebe a nossa lista de servicos
+	public RelatorioTableModel(List<RelatorioStatus> valores) {
+		this.valores = new ArrayList<RelatorioStatus>(valores);
+	}
+
+	public int getRowCount() {
+		// Quantas linhas tem sua tabela? Uma para cada item da lista.
+		return valores.size();
+	}
+
+	public int getColumnCount() {
+		// Quantas colunas tem a tabela? Nesse exemplo, só 2.
+		return 4;
+	}
+
+	public String getColumnName(int column) {
+		// Qual é o nome das nossas colunas?
+		if (column == COL_CLIENTE)
+			return "Cliente";
+		if (column == COL_STATUS)
+			return "Status";
+		if (column == COL_DATA)
+			return "Data";
+		if (column == COL_VALOR)
+			return "Valor";
+		return ""; // Nunca deve ocorrer
+	}
+
+	public Object getValueAt(int row, int column) {
+		// Precisamos retornar o valor da coluna column e da linha row.
+		RelatorioStatus rs = valores.get(row);
+		if (column == COL_CLIENTE)
+			return rs.getCliente().getNome();
+		else if (column == COL_STATUS)
+			return rs.getStatus();
+		else if (column == COL_DATA)
+			return rs.getData();
+		else if (column == COL_VALOR)
+			return rs.getValor();
+		return ""; // Nunca deve ocorrer
+	}
+
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		RelatorioStatus rs = valores.get(rowIndex);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		// Vamos alterar o valor da coluna columnIndex na linha rowIndex com o
+		// valor aValue passado no parâmetro.
+		// Note que vc poderia alterar 2 campos ao invés de um só.
+		if (columnIndex == COL_CLIENTE)
+			rs.getCliente().setNome(aValue.toString());
+		else if (columnIndex == COL_STATUS)
+			rs.setStatus(Integer.parseInt(aValue.toString()));
+		else if (columnIndex == COL_DATA)
+			try {
+				rs.setData(sdf.parse(aValue.toString()));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		else if (columnIndex == COL_VALOR)
+			rs.setValor(Double.parseDouble(aValue.toString()));
+
+	}
+
+	public Class<?> getColumnClass(int columnIndex) {
+		// Qual a classe das nossas colunas? Como estamos exibindo texto, é
+		// string.
+		return String.class;
+	}
+
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		// Indicamos se a célula da rowIndex e da columnIndex é editável. Nossa
+		// tabela toda é.
+		return true;
+	}
+
+	// Já que esse tableModel é de servicos, vamos fazer um get que retorne um
+	// objeto servico inteiro.
+	// Isso elimina a necessidade de chamar o getValueAt() nas telas.
+	public RelatorioStatus get(int row) {
+		return valores.get(row);
+	}
+}
--- src/br/senai/sc/view/ConsultaClientesOrcUI.java
+++ src/br/senai/sc/view/ConsultaClientesOrcUI.java
@@ -37,7 +37,7 @@ public class ConsultaClientesOrcUI extends JInternalFrame {
 	private JTextField jtfFiltro;
 	private JTable table;
 	private JTable jtConsultaCliente;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -58,37 +58,40 @@ public class ConsultaClientesOrcUI extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public ConsultaClientesOrcUI() {
-		setFrameIcon(new ImageIcon(ConsultaClientesOrcUI.class.getResource("/br/senai/sc/icons/fornecedores.png")));
+		setFrameIcon(new ImageIcon(
+				ConsultaClientesOrcUI.class
+						.getResource("/br/senai/sc/icons/fornecedores.png")));
 		setTitle("Consultar Clientes");
 		setBorder(null);
 		getContentPane().setBackground(SystemColor.inactiveCaption);
 		setBounds(2, 40, 1192, 500);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		panel.setBackground(SystemColor.inactiveCaption);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		
+
 		JButton jbSelecionar = new JButton("Selecionar");
 		jbSelecionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Cliente cli = null;
 				try {
-					cli = (Cliente) new ClienteTableModel(
-							new ClienteControl().showAllClientes())
-								.get(jtConsultaCliente.getSelectedRow());
-					 new OrcamentoFlashControl().inserirCliente(cli);
-						dispose();
-				} catch (ClassNotFoundException | SQLException | ArrayIndexOutOfBoundsException e) {
+					cli = (Cliente) new ClienteTableModel(new ClienteControl()
+							.showAllClientes()).get(jtConsultaCliente
+							.getSelectedRow());
+					new OrcamentoFlashControl().inserirCliente(cli);
+					dispose();
+				} catch (ClassNotFoundException | SQLException
+						| ArrayIndexOutOfBoundsException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 
 			}
 		});
-		
+
 		JButton jbCancelar = new JButton("Cancelar");
 		jbCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -96,38 +99,70 @@ public class ConsultaClientesOrcUI extends JInternalFrame {
 			}
 		});
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 1171, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 1175, GroupLayout.PREFERRED_SIZE)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(jbSelecionar, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(jbCancelar, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(15, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 222, GroupLayout.PREFERRED_SIZE)
-					.addGap(65)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(jbSelecionar)
-						.addComponent(jbCancelar))
-					.addContainerGap(198, Short.MAX_VALUE))
-		);
-		
+		groupLayout
+				.setHorizontalGroup(groupLayout
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								groupLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addComponent(
+																scrollPane,
+																GroupLayout.PREFERRED_SIZE,
+																1171,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																panel,
+																GroupLayout.PREFERRED_SIZE,
+																1175,
+																GroupLayout.PREFERRED_SIZE)
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				jbSelecionar,
+																				GroupLayout.PREFERRED_SIZE,
+																				219,
+																				GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				ComponentPlacement.UNRELATED)
+																		.addComponent(
+																				jbCancelar,
+																				GroupLayout.PREFERRED_SIZE,
+																				219,
+																				GroupLayout.PREFERRED_SIZE)))
+										.addContainerGap(15, Short.MAX_VALUE)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(
+				Alignment.LEADING)
+				.addGroup(
+						groupLayout
+								.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(panel,
+										GroupLayout.PREFERRED_SIZE, 43,
+										GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(scrollPane,
+										GroupLayout.PREFERRED_SIZE, 222,
+										GroupLayout.PREFERRED_SIZE)
+								.addGap(65)
+								.addGroup(
+										groupLayout
+												.createParallelGroup(
+														Alignment.BASELINE)
+												.addComponent(jbSelecionar)
+												.addComponent(jbCancelar))
+								.addContainerGap(198, Short.MAX_VALUE)));
+
 		jtConsultaCliente = new JTable();
 		scrollPane.setViewportView(jtConsultaCliente);
 		try {
-			jtConsultaCliente.setModel(new ClienteTableModel( 
-					new ClienteControl().showAllClientes() ) );
+			jtConsultaCliente.setModel(new ClienteTableModel(
+					new ClienteControl().showAllClientes()));
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -144,34 +179,35 @@ public class ConsultaClientesOrcUI extends JInternalFrame {
 		jtConsultaCliente.getColumnModel().getColumn(2).setResizable(false);
 		jtConsultaCliente.getColumnModel().getColumn(3).setPreferredWidth(100);
 		jtConsultaCliente.getColumnModel().getColumn(3).setResizable(false);
-		getContentPane().setLayout(groupLayout);		
-		
-		
+		getContentPane().setLayout(groupLayout);
+
 		final JLabel lblFiltro = new JLabel("Filtro");
-		
+
 		jtfFiltro = new JTextField();
 		jtfFiltro.setColumns(10);
-		
+
 		JLabel lblTipoFiltro = new JLabel("Tipo filtro");
-		
+
 		final JComboBox jcbTipoFiltro = new JComboBox();
 		jcbTipoFiltro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				jtfFiltro.setText(null);
 			}
 		});
-		jcbTipoFiltro.setModel(new DefaultComboBoxModel(new String[] {"Nome", "CPF", "Email", "Telefone"}));
-		
-		
-		
+
+		jcbTipoFiltro.setModel(new DefaultComboBoxModel(new String[] { "Nome",
+				"CPF", "Email", "Telefone" }));
+
 		JButton btnNewButton_1 = new JButton("Procurar");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Cliente cli = new Cliente();
 				try {
-					jtConsultaCliente.setModel(new ClienteTableModel( 
-							new ClienteControl().showFilterClientes(jcbTipoFiltro.getSelectedItem().toString() ,jtfFiltro.getText() ) ) );
+					jtConsultaCliente.setModel(new ClienteTableModel(
+							new ClienteControl().showFilterClientes(
+									jcbTipoFiltro.getSelectedItem().toString(),
+									jtfFiltro.getText())));
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -182,33 +218,48 @@ public class ConsultaClientesOrcUI extends JInternalFrame {
 			}
 		});
 		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(20)
-					.addComponent(lblFiltro)
-					.addGap(10)
-					.addComponent(jtfFiltro, GroupLayout.PREFERRED_SIZE, 625, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 78, Short.MAX_VALUE)
-					.addComponent(lblTipoFiltro)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(jcbTipoFiltro, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
-					.addGap(76)
-					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
-					.addGap(30))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(
+				Alignment.LEADING).addGroup(
+				gl_panel.createSequentialGroup()
+						.addGap(20)
 						.addComponent(lblFiltro)
-						.addComponent(jtfFiltro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_1)
+						.addGap(10)
+						.addComponent(jtfFiltro, GroupLayout.PREFERRED_SIZE,
+								625, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED, 78,
+								Short.MAX_VALUE)
 						.addComponent(lblTipoFiltro)
-						.addComponent(jcbTipoFiltro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(8))
-		);
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(jcbTipoFiltro,
+								GroupLayout.PREFERRED_SIZE, 124,
+								GroupLayout.PREFERRED_SIZE)
+						.addGap(76)
+						.addComponent(btnNewButton_1,
+								GroupLayout.PREFERRED_SIZE, 129,
+								GroupLayout.PREFERRED_SIZE).addGap(30)));
+		gl_panel.setVerticalGroup(gl_panel
+				.createParallelGroup(Alignment.TRAILING)
+				.addGroup(
+						gl_panel.createSequentialGroup()
+								.addContainerGap(GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE)
+								.addGroup(
+										gl_panel.createParallelGroup(
+												Alignment.BASELINE)
+												.addComponent(lblFiltro)
+												.addComponent(
+														jtfFiltro,
+														GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE,
+														GroupLayout.PREFERRED_SIZE)
+												.addComponent(btnNewButton_1)
+												.addComponent(lblTipoFiltro)
+												.addComponent(
+														jcbTipoFiltro,
+														GroupLayout.PREFERRED_SIZE,
+														GroupLayout.DEFAULT_SIZE,
+														GroupLayout.PREFERRED_SIZE))
+								.addGap(8)));
 		panel.setLayout(gl_panel);
 		getContentPane().setLayout(groupLayout);
 
